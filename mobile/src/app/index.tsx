573e2efaee47935322db9ce4c8c26a106771e163
Ruan <133697229+RuanFioriMarcelino@users.noreply.github.com>
2024-05-15 22:26:22 -0300

    Add files via upload

---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react"
+import {View, Image, StatusBar, Alert} from "react-native"
+import { MaterialCommunityIcons } from "@expo/vector-icons"
+import { api } from "@/server/api"
+
+import { colors } from "@/styles/colors"
+import { useBadgeStore } from "@/store/badge-store"
+
+import { Input } from "@/components/input"
+import { Button } from "@/components/button"
+
+import { Link, Redirect } from "expo-router"
+
+export default function Home(){
+    const [code, setCode] = useState('');
+    const [isLoading, setIsLoading] = useState(false)
+
+    const badgeStore = useBadgeStore()
+
+    async function handleAccessCredential(){
+        try{
+        if (!code.trim()) {
+            return Alert.alert("Ingresso","Informe o código do ingresso!")
+        }
+
+        setIsLoading(true)
+
+        const { data } = await api.get(`/attendees/${code}/badge`)
+        badgeStore.save(data.badge)
+        }catch(error){
+            console.log(error)
+            setIsLoading(false)
+
+            Alert.alert("Ingresso","Ingresso não encontrado!")
+        }
+    }
+
+    if (badgeStore.data?.checkInURL){
+        return <Redirect href={"/ticket"} />
+    }
+
+    return(
+        <View className="flex-1 bg-green-500 items-center justify-center p-8">
+            <StatusBar barStyle={"light-content"}/>
+            <Image 
+            source={require("@/assets/logo.png")} 
+            className="h-16" 
+            resizeMode="contain"
+            />
+
+        <View className="w-full mt-12 gap-3">
+            <Input>
+            <MaterialCommunityIcons 
+                name="ticket-confirmation-outline" 
+                color={colors.green[200]}
+                size={20}
+            />
+            <Input.Field placeholder="Código do Ingresso"
+            onChangeText={setCode}/>
+            </Input>
+
+            <Button title="Acessar Credencial" onPress={handleAccessCredential} isLoading={isLoading}    />
+            
+            <Link href={"/register"} className="text-gray-100 text-base font-bold text-center mt-8">Ainda não possui ingresso?</Link>
+        </View>
+        </View>
+    )
+}
